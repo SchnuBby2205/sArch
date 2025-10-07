7c666945cae4ac16c1f47e5e6a7b4375822f9278
SchnuBby2205 <80288097+SchnuBby2205@users.noreply.github.com>
2025-10-07 14:50:58 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-scriptname=$(basename "$0")
-logFile="/var/log/ArchInstall.log"
-RED="\e[31m" GREEN="\e[32m" YELLOW="\e[1;33m" WHITE="\e[1;37m" NC="\e[0m"
-CROSS="\u2717" CHECK="\u2713"
-RUNNING="${YELLOW}•${NC}" ERROR="${RED}${CROSS}${NC}" MYOK="${GREEN}${CHECK}${NC}"
-UP="\e[A" CLEAR="\r                                        \r"
+
+export sARCH_MAIN="."
+export sARCH_SCRIPTS="$sARCH_MAIN/scripts"
+
+source "$sARCH_SCRIPTS/constants.sh"
+
 Banner() {
   clear
   myPrint print green ".▄▄ ·  ▄▄·  ▄ .▄ ▐ ▄ ▄• ▄▌▄▄▄▄· ▄▄▄▄·  ▄· ▄▌            \n"

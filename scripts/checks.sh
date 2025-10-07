e2bcf450c87affcfe5ae0b853f71416d1c2a6186
SchnuBby2205 <80288097+SchnuBby2205@users.noreply.github.com>
2025-10-07 14:55:40 +0200

    Create checks.sh

---
+++ scripts/checks.sh
@@ -0,0 +1,12 @@
+## Check functions for input partitions flags etc
+runCFDiskIfNeeded(){ 
+  [[ -z "$cfdisk" && -n "$disk" ]] && getInput "\nStart cfdisk (y/N) ?\n" cfdisk "N"
+  [[ "$cfdisk" =~ ^[yY]$ ]] && { 
+    [[ -z "$disk" ]] && { getInput "\nEnter disk\n" disk; [[ -z "$disk" ]] && exitWithError "No disk entered -> exit\n"; }
+    cfdisk "$disk"
+  }
+}
+validatePartition() { lsblk -o NAME|grep -qw "${1#/dev/}"||{ exitWithError "$1: Partition does not exist!"; }; }
+checkPartitions() { [[ -z "$boot" ]] && getInput "Enter boot partition: " boot; [[ -z "$swap" ]] && getInput "Enter swap partition: " swap; [[ -z "$root" ]] && getInput "Enter root partition: " root; }
+validateUser() { [[ "$1" =~ ^[a-z_][a-z0-9_-]*$ ]] || exitWithError "Invalid username!"; }
+checkDebugFlag() { debugstring=$([[ "$debug" =~ ^[yY]$ ]] && echo "" || echo " &>/dev/null"); }

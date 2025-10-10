02767bf2d79fe0f4e8a088a9b832e119cd39b2d4
SchnuBby2205 <dominik.muche@outlook.com>
2025-10-10 16:17:49 +0200

    readList does work now with strings

--- scripts/installs.sh
+++ scripts/installs.sh
@@ -69,7 +69,9 @@ installDE() { Banner; checkDebugFlag
 installConfigs() { Banner; checkDebugFlag
   sudo pacman -Syy $debugstring
   [[ "$debug" == false ]] && myPrint step Running "Final steps..."
-    readList "$sARCH_CONFIGS/$gpu"; dryRun runCMDS 0 Installing $name 0 $value 20 "$pacmanRun ${list[@]} $debugstring"
+    readList "$sARCH_CONFIGS/$gpu"
+    install="${list[@]}"
+    runCMDS 0 Installing $name 0 $value 20 "$pacmanRun $install $debugstring"
     dryRun runCMDS 0 Installing "dxvk-bin..." 5 10 20 "yay -S --noconfirm dxvk-bin $debugstring"
     safeCMD mv $HOME/.config/hypr $HOME/.config/hypr_bak; safeCMD mv $HOME/$sARCH_CONFIGS/hypr $HOME/.config/hypr
     dryRun runCMDS 0 Installing STEAM... 12 17 20 "steam $debugstring"

2fe0a2fd5a224d5dcb1127538da2d4d2bd5b2e39
SchnuBby2205 <dominik.muche@outlook.com>
2025-10-10 15:59:24 +0200

    more Banners!

--- scripts/installs.sh
+++ scripts/installs.sh
@@ -52,7 +52,7 @@ installDE() { Banner; checkDebugFlag
   makepkg -si
   cd ..
   rm -rf ./yay
-  printf "\n"
+  Banner
   [[ "$debug" == false ]] && myPrint step Running "Post install..."
     runCMDS 1 Creating "SDDM config directory..." 0 2 20 "sudo mkdir /etc/sddm.conf.d"
     runCMDS 0 Installing Quickshell... 2 15 20 "yay -S quickshell --noconfirm $debugstring"

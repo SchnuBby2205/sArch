0f7e1e1cf0dd4d64123bdb4f85e01e77c974a586
SchnuBby2205 <dominik.muche@outlook.com>
2025-10-10 14:41:18 +0200

    spacebars removed

--- scripts/installs.sh
+++ scripts/installs.sh
@@ -40,7 +40,7 @@ installDE() { Banner; checkDebugFlag
   bash -c "sudo pacman -Syy $debugstring"
   myPrint countdown 3 "Starting installation in"
   [[ "$debug" == false ]] && myPrint step Installing "Dependencies..."
-    s=0; for r in systemdeps audiodeps programs fonts; do readList "$sARCH_INSTALLCONFIGS/$r"; dryRun runCMDS 0 Installing $name $s $value 20 "$pacmanRun ${list[@]} $debugstring"; s=$value; done
+    s=0; for r in systemdeps audiodeps programs fonts; do readList "$sARCH_INSTALLCONFIGS/$r"; dryRun runCMDS 0 Installing "$name" $s $value 20 "$pacmanRun ${list[@]} $debugstring"; s=$value; done
   [[ "$debug" == false ]] && myPrint step ok
   safeCMD git clone https://aur.archlinux.org/yay.git $debugstring; cd yay; safeCMD makepkg -si; cd ..; safeCMD rm ./yay; printf "\n"
   [[ "$debug" == false ]] && myPrint step Running "Post install..."

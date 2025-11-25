c541405ee21d808755170718180a63c01b3eea7c
SchnuBby2205 <dominik.muche@outlook.com>
2025-11-25 19:00:45 +0100

    new caching for wallpapers and rofi

--- bin/sarch_change_wallpaper.sh
+++ bin/sarch_change_wallpaper.sh
@@ -14,7 +14,8 @@ wallpaper=$(for a in *.jpg *.png; do
 done | rofi -dmenu -theme gruvbox-material_icons.rasi)
 
 matugen image ${wallpaperDir}${wallpaper}
-magick "${wallpaperDir}${wallpaper}" -thumbnail 1000x1000^ -gravity center -extent 1000x1000 -quality 100 "${cacheDir}curr"
+magick "${wallpaperDir}${wallpaper}" -thumbnail 500x500^ -gravity center -extent 500x500 -quality 70 "${cacheDir}curr"
+magick "${wallpaperDir}${wallpaper}" -thumbnail 1000x500^ -gravity center -extent 1000x500 -quality 70 "${cacheDir}curr_wide"
 sed -i ':a;$!N;$!ba;s/,\s*}/}/' ~/.config/quickshell/myShell/Configs/colors.json
 
 cd $returnDir

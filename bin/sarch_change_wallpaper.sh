9206bdcdc0f1ebe5487ccf72ccbefda5782d08f9
SchnuBby2205 <dominik.muche@outlook.com>
2026-04-07 10:21:32 +0200

    New Matugen
    color selector for matugen

--- bin/sarch_change_wallpaper.sh
+++ bin/sarch_change_wallpaper.sh
@@ -13,10 +13,35 @@ wallpaper=$(for a in *.jpg *.png; do
     #echo $a
 done | rofi -dmenu -theme gruvbox-material_icons.rasi)
 
-matugen image ${wallpaperDir}${wallpaper}
+tmpfile=$(mktemp)
+script -q -c "timeout 1 matugen image '${wallpaperDir}${wallpaper}' --dry-run 2>&1; true" "$tmpfile" > /dev/null
+colors=$(cat "$tmpfile" | grep -oP '#[0-9A-Fa-f]{6}' | head -5)
+rm "$tmpfile"
+
+# Farbige Icons generieren und rofi-Einträge bauen
+icondir=$(mktemp -d)
+rofi_input=""
+i=0
+while IFS= read -r color; do
+    iconfile="${icondir}/${i}.png"
+    magick -size 32x32 xc:"${color}" "${iconfile}"
+    rofi_input+="${i}: ${color}\0icon\x1f${iconfile}\n"
+    ((i++))
+done <<< "$colors"
+
+selected=$(echo -e "$rofi_input" | rofi -dmenu -p "Source Color" -theme gruvbox-material_icons.rasi)
+color_index=$(echo "$selected" | grep -oP '^\d+')
+color_index=${color_index:-0}
+
+rm -rf "$icondir"
+
+matugen image "${wallpaperDir}${wallpaper}" --source-color-index "${color_index}"
+
+#matugen image ${wallpaperDir}${wallpaper} --source-color-index 0
+
 magick "${wallpaperDir}${wallpaper}" -thumbnail 500x500^ -gravity center -extent 500x500 -quality 70 "${cacheDir}curr"
 magick "${wallpaperDir}${wallpaper}" -thumbnail 1000x500^ -gravity center -extent 1000x500 -quality 70 "${cacheDir}curr_wide"
-sed -i ':a;$!N;$!ba;s/,\s*}/}/' ~/.config/quickshell/myShell/Configs/colors.json
+#sed -i ':a;$!N;$!ba;s/,\s*}/}/' ~/.config/quickshell/myShell/Configs/colors.json
 
 if [[ -n "$wallpaper" ]]; then
     dunstify -a "Themes" -u low -t 1000 -c "Wallpaper changed" "${wallpaper}"
